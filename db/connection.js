898728cebeff5025dc107d5fb577d83d9b3c8a5a
Jon Ortiz <jonnatano94@gmail.com>
2024-02-28 15:56:21 -0800

    Add initial mongoDB set up connection file

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import 'dotenv/config.js';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }, 6000000);
+        console.log('Success: MongoDB connected!');
+
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB has disconnected!');
+        });
+
+        mongoose.connection.on('error', (error) => {
+            console.log(`There was an error connecting to MongoDB: ${error.message}`);
+        });
+    } catch (error) {
+        console.log(`Failed: MongoDB connection error: ${error}`)
+        process.exit(1);
+    }
+}
+export default connectDB;
