ac5d21fdc2cecba5a9c139c3500c68ffb4382c31
Jon Ortiz <jonnatano94@gmail.com>
2024-02-28 15:56:52 -0800

    Add Review Model

---
+++ models/Review.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+const reviewSchema = new mongoose.Schema({
+    // id: Number,
+    id: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    source: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    time_created: {
+        type: Date,
+        required: true,
+    },
+    user: {
+        id: {
+            type: String,
+            required: true,
+        },
+        profile_url: {
+            type: String,
+            required: true,
+        },
+        image_url: {
+            type: String,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+export default Review;
