cbda1573679a0adaa2d38ac36fe0f3c5fede7ba7
Jon Ortiz <jonnatano94@gmail.com>
2024-03-06 18:19:19 -0800

    Changes to server file for CORS update

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ import reviewsRoutes from './routes/reviewsRoutes.js';
 import 'dotenv/config.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
+
+// Enable CORS for GET route
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
 
 app.use(bodyParser.json());
 
