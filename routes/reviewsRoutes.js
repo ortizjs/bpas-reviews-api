a1c6f26bdd1b3e127496ecacd106b725cf00e26f
Jon Ortiz <jonnatano94@gmail.com>
2024-03-08 10:19:19 -0800

    Update routes file

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/', reviewsController.root);
 router.get('/reviews', reviewsController.listReviews);
+router.post('/review', reviewsController.addReview);
 router.get('/fetch_yelp_reviews', reviewsController.fetchYelpReviews);
 router.get('/fetch_google_reviews', reviewsController.fetchGoogleReviews);
 
